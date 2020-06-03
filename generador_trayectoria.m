6bed2b4ad848c134e6dc306b0dcf9727e0d24b27
mariajesusmoragarcia <61427246+mariajesusmoragarcia@users.noreply.github.com>
2020-06-03 08:29:43 -0700

    Add files via upload

--- generador_trayectoria.m
+++ generador_trayectoria.m
@@ -3,21 +3,28 @@
 % de conjuntos de puntos y la orientación de la trayectria. 
 % tray = [pos, anlge-axis]. GENERADOR DE TRAYECTORIAS.
 
-function trayectoria = generador_trayectoria(p0, r1, r2, rotv)
+function trayectoria = generador_trayectoria(p0, r1, r2, eul)
     close all;
     
-    % Nos aseguramos que la pose p0 sean 1x3
+    % Nos aseguramos de que la pose p0 sean 1x3
     if size(p0,1) ~= 3
          error('p0 is not 3x1 vector');
     end
 
-    % Nos aseguramos que el vector de orientación sea 1x3
-    if size(rotv,1) ~= 1
+    % Nos aseguramos de que el vector de orientación sea 1x3
+    if size(eul,1) ~= 1
         error('eul is not 1x3 vector');
     end   
     
+    % Nos aseguramos de que los radios no sean negativos ni = 0
+    if r1 <= 0 || r2 <= 0
+        error('r1 o r2 <= 0');
+    end   
+   
+    
     % Matriz de orientación
-    rotm = rotationVectorToMatrix(rotv)
+    rotm = eul2rotm(eul);
+    %rotm = rotationVectorToMatrix(rotv)
     
     % Punto inicial
     x0 = p0(1);
@@ -29,22 +36,27 @@ function trayectoria = generador_trayectoria(p0, r1, r2, rotv)
        
     % Sistema de referencia
     T = [[rotm, pini]; [0 0 0 1]];
-    createFRAME(T,'m','To',2);
+    createFRAME(T,'m','To',0.03);
+    T0 = [[rotm,[0;0;0]]; [0 0 0 1]];
+    createFRAME(T0,'y','T0',0.03);
    
-    t = 0:pi/200:2*pi;
+    % Frecuencia y periodo de muestreo
+    f = 125; %Hz
+    T = 1/f;
+    t = 0:T:2*pi;
     
     % Calculamos el centro de la elipse
-    xC = x0 - r1*cos(0);
-    yC = y0 - r2*sin(0);
-    zC = 0;
+    Cx = x0 - r1*cos(0);
+    Cy = y0 - r2*sin(0);
+    Cz = z0;
     
-    C = rotm*[xC; yC; zC];
+    C = rotm*[Cx; Cy; Cz];
     plot3(C(1), C(2), C(3), 'b*')
 
     % Calculamos el array de puntos de la elipse sin orientacion
     for i = 1:length(t)
-        x_(i) = xC - r1*cos(i);
-        y_(i) = yC - r2*sin(i);
+        x_(i) = Cx + r1*cos(i);
+        y_(i) = Cy + r2*sin(i);
         z_(i) = z0;
         
         % Calculamos el array de puntos de la elipse CON orientacion.
@@ -54,9 +66,9 @@ function trayectoria = generador_trayectoria(p0, r1, r2, rotv)
         angle_axis(:,i) = rotationMatrixToVector(rotm); 
     end
     
-    plot3(pos(1,:),pos(2,:),pos(3,:),'g.'); grid; title('Trayectoria generada');
+    plot3(pos(1,:),pos(2,:),pos(3,:),'g.'); grid; title('Trayectoria planificada');
     xlabel('EJE X') 
     ylabel('EJE Y') 
     zlabel('EJE Z')
-    trayectoria = [pos; angle_axis]
+    trayectoria = [pos; angle_axis];
 end
